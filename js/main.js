d3d9d5100de3e1c026cacf427cd37c3bd6f11128
Maciej Loch <maqloch@gmail.com>
2016-04-09 16:47:55 +0200

    no private data for bots

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 (function(){
     'use strict';
+    var dtr = $('.dtr');
     var contactBtn = $('#contact-btn');
     var contact = $('#contact');
+
+    var _data = {
+        telefon: '721 517 581',
+        email: 'alina.blizniuk@onet.pl'
+    };
+
+    $.each(_data, function (key, value) {
+        dtr.html(function (i, oldHtml) {
+            return oldHtml.replace(new RegExp('%'+key+'%', 'g'), value);
+        });
+    });
 
     contactBtn.click(function(e){
         e.preventDefault();
