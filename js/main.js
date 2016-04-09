930cc182775243c3ef8c06e90f5106867e04a68d
Maciej Loch <maqloch@gmail.com>
2016-04-09 16:09:10 +0200

    analytics and speed adjustments

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
     var contactBtn = $('#contact-btn');
     var contact = $('#contact');
 
-
     contactBtn.click(function(e){
         e.preventDefault();
         $('html, body').animate({scrollTop: contact.offset().top}, 'slow');
